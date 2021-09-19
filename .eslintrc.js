797fc1f911786ccfae4830d836b8633a08355169
Jacano <641283231@qq.com>
2021-09-19 17:50:17 +0800

    chore: modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,26 @@
 module.exports = {
   root: true,
   env: {
-		es6: true,
-		node: true,
-		browser: true,
-		jest: true
-	},
-  extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier/@typescript-eslint"
-	],
-	rules: {
-		"no-empty-function": 0,
-		"@typescript-eslint/no-explicit-any": 0,
-		"@typescript-eslint/no-empty-function": 0,
-		"@typescript-eslint/explicit-function-return-type": 0,
-		"@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": false }],
-    "prefer-const": 0,
-    "@typescript-eslint/interface-name-prefix": 0
-	},
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'no-empty-function': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false }],
+    'prefer-const': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
   parserOptions: {
     ecmaVersion: 6,
-    parser: "babel-eslint",
-    sourceType: "module",
-	},
-	parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-};
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+}
